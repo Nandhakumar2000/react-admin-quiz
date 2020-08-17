55dffce944b4a1d71809bb3b5f6514e5aa10cf5e
Nandhakumar V <nandhakumar@Nandhus-MacBook-Pro.local>
2020-08-17 22:05:39 +0530

    Updates

---
+++ precache-manifest.9d80e040e3701b091469fff6af450a09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4afb48dc6d8528ce2ff817767f76eec5",
+    "url": "/react-admin-quiz/index.html"
+  },
+  {
+    "revision": "ebf73431755b862e4db0",
+    "url": "/react-admin-quiz/static/css/2.6f222376.chunk.css"
+  },
+  {
+    "revision": "ad83a158cf66bbbb0ca0",
+    "url": "/react-admin-quiz/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ebf73431755b862e4db0",
+    "url": "/react-admin-quiz/static/js/2.7b62c874.chunk.js"
+  },
+  {
+    "revision": "0042ed35eedf85f73c564b78f2a06746",
+    "url": "/react-admin-quiz/static/js/2.7b62c874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad83a158cf66bbbb0ca0",
+    "url": "/react-admin-quiz/static/js/main.69a95da5.chunk.js"
+  },
+  {
+    "revision": "c184d5b5c426858577d8",
+    "url": "/react-admin-quiz/static/js/runtime-main.cebd6e09.js"
+  }
+]);
